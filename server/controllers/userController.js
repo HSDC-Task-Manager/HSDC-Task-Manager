e7f0eb1a14da3aad5f7945fa12c0bd317538b9e8
Derrick <juneparkoh@gmail.com>
2023-03-08 16:30:38 -0800

    installed pg, concurrently, win-node-env
    Co-authored-by: csuzukida <chris.suzukida@gmail.com>
    Co-authored-by: Igniphis <juneparkoh@gmail.com>
    Co-authored-by: a-t-klein <alxtkln@gmail.com>
    Co-authored-by: DavidMNorman <DavidMNorman@gmail.com>
    Co-authored-by: n8ngo <nathanngo8796@gmail.com>

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -66,24 +66,24 @@ userController.createUser = async (req, res, next) => {
 
 //TODO test parameterized query to ensure it works. - NN
 // SELECT EXITS query will return a boolean value
-userController.verifyUser = async (req, res, next) {
+userController.verifyUser = async (req, res, next) => {
   try {
     const { username, password } = req.body;
     const userVals = [username, password];
     const query = await db.query(
       "SELECT id FROM users WHERE user_name=$1 AND password=$2;",
       userVals
-    )
+    );
     res.locals.verifiedUser = query !== [];
     res.locals.id = query.rows[0].id;
     return next();
   } catch (error) {
     return next({
       log: "userController.verifyUser",
-      message: {err: "userController.verifyUser" + error}
-    })
+      message: { err: "userController.verifyUser" + error },
+    });
   }
-}
+};
 
 /* * * *
  * TODO - can add any of the following we wish to have stored in the session as well:
@@ -115,15 +115,18 @@ userController.verifyUser = async (req, res, next) {
 userController.getBoardIds = async (req, res, next) => {
   try {
     const { userID } = req.body;
-    const query = await db.query('SELECT boards.id, boards.name FROM workspace INNER JOIN users ON workspace.user_id = users.id INNER JOIN boards ON workspace.board_id = boards.id WHERE workspace.user_id = $1;', userID)
+    const query = await db.query(
+      "SELECT boards.id, boards.name FROM workspace INNER JOIN users ON workspace.user_id = users.id INNER JOIN boards ON workspace.board_id = boards.id WHERE workspace.user_id = $1;",
+      userID
+    );
     //note, only index 0 while we only have 1 board
     res.locals.boardID = query.rows[0].id;
-  } catch(error) {
+  } catch (error) {
     return next({
       log: "error in userController.getBoardIds",
       message: { err: "userController.getBoardIds" + error },
-    })
+    });
   }
-}
+};
 
 module.exports = userController;
